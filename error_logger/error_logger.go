d5ab8120bcfed928aa670bc2adf88e921d6100fd
Julien Levesy <jlevesy@gmail.com>
2015-09-21 14:09:00 -0700

    add the Setup function in error_logger

--- error_logger/error_logger.go
+++ error_logger/error_logger.go
@@ -1,7 +1,18 @@
 package error_logger
 
+import "log"
+
 type Options map[string]interface{}
 
 type ErrorLogger interface {
 	Capture(error, *Options) error
+}
+
+func Setup(logger ErrorLogger) {
+	if e := recover(); e != nil {
+		if err, ok := e.(error); ok {
+			logger.Capture(err, nil)
+			log.Fatalf(err.Error())
+		}
+	}
 }
