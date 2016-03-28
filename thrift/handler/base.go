d5557e1943800a7f9f06e57773f92be652bbb48c
Alexis Montagne <alexis.montagne@gmail.com>
2016-03-28 16:34:15 -0700

    Declare Version number as constants

--- thrift/handler/base.go
+++ thrift/handler/base.go
@@ -1,6 +1,9 @@
 package handler
 
 import (
+	"strconv"
+	"strings"
+
 	"github.com/upfluence/goutils/Godeps/_workspace/src/github.com/upfluence/base"
 	"github.com/upfluence/goutils/Godeps/_workspace/src/github.com/upfluence/base/base_service"
 	"github.com/upfluence/goutils/Godeps/_workspace/src/github.com/upfluence/base/version"
@@ -8,6 +11,12 @@ import (
 
 const (
 	BASE_EXCHANGE_NAME = "monitoring-exchange"
+
+	GitCommit = ""
+	GitBranch = ""
+	GitRemote = ""
+
+	Version = "v0.0.0"
 )
 
 type Interface interface {
@@ -22,12 +31,32 @@ type Base struct {
 	Interfaces []Interface
 }
 
+func buildSemanticVersion() *version.SemanticVersion {
+	splittedVersion := strings.Split(Version, ".")
+
+	if len(splittedVersion) != 3 {
+		return nil
+	}
+
+	major, _ := strconv.Atoi(splittedVersion[0][1:])
+	minor, _ := strconv.Atoi(splittedVersion[1])
+	patch, _ := strconv.Atoi(splittedVersion[2])
+
+	return &version.SemanticVersion{int16(major), int16(minor), int16(patch)}
+}
+
 func (h *Base) GetName() (string, error) {
 	return h.UnitName, nil
 }
 
 func (h *Base) GetVersion() (*version.Version, error) {
-	return h.Version, nil
+	var gitVersion *version.GitVersion
+
+	if GitCommit != "" {
+		gitVersion = &version.GitVersion{GitCommit, GitRemote, GitBranch}
+	}
+
+	return &version.Version{buildSemanticVersion(), gitVersion}, nil
 }
 
 func (h *Base) GetInterfaceVersions() (map[string]*version.Version, error) {
