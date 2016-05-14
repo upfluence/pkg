e5d8ab2702cabfd52d42ca88052594c5d5d4f018
Alexis Montagne <alexis.montagne@gmail.com>
2016-05-13 17:22:03 -0700

    Add helper to build a gocassa connection

---
+++ testing/cassandra/gocassa.go
@@ -0,0 +1,72 @@
+package cassandra
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+
+	"github.com/hailocab/gocassa"
+	_ "github.com/upfluence/goutils/Godeps/_workspace/src/github.com/mattes/migrate/driver/cassandra"
+	"github.com/upfluence/goutils/Godeps/_workspace/src/github.com/mattes/migrate/migrate"
+)
+
+const (
+	defaultCassandraIP = "127.0.0.1"
+	defaultKeyspace    = "test"
+	cassandraPort      = 9042
+	protocolVersion    = 3
+)
+
+func BuildKeySpace(migrationsPath *string) (gocassa.KeySpace, error) {
+	var (
+		cassandraIP = os.Getenv("CASSANDRA_IP")
+		keySpace    = os.Getenv("CASSANDRA_KEY_SPACE")
+	)
+
+	if cassandraIP == "" {
+		cassandraIP = defaultCassandraIP
+	}
+
+	if keySpace == "" {
+		keySpace = fmt.Sprintf("%s%d", defaultKeyspace, rand.Int31())
+	}
+
+	conn, err := gocassa.Connect([]string{cassandraIP}, "", "")
+
+	if err != nil {
+		return nil, err
+	}
+
+	conn.DropKeySpace(keySpace)
+	conn.CreateKeySpace(keySpace)
+
+	if migrationsPath != nil {
+		errs, ok := migrate.UpSync(
+			fmt.Sprintf(
+				"cassandra://%s:%d/%s?protocol=%d",
+				cassandraIP,
+				cassandraPort,
+				keySpace,
+				protocolVersion,
+			),
+			*migrationsPath,
+		)
+
+		if !ok {
+			strErrs := []string{}
+			for _, migrationError := range errs {
+				strErrs = append(strErrs, migrationError.Error())
+			}
+
+			conn.Close()
+			return nil, errors.New(strings.Join(strErrs, ","))
+		}
+	}
+
+	ks := conn.KeySpace(keySpace)
+	ks.DebugMode(true)
+
+	return ks, nil
+}
