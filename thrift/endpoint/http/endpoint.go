da44d157255d43e2a3f8d477eb9b753d41b0ccf4
Alexis Montagne <alexis.montagne@gmail.com>
2017-03-21 14:01:37 -0400

    Return error on NewEndpoint too

--- thrift/endpoint/http/endpoint.go
+++ thrift/endpoint/http/endpoint.go
@@ -18,9 +18,13 @@ type Endpoint struct {
 	mux     *http.ServeMux
 }
 
-func NewEndpoint(baseHandler *handler.Base, port int) *Endpoint {
+func NewEndpoint(baseHandler *handler.Base, port int) (*Endpoint, error) {
 	mux := http.NewServeMux()
-	trans, _ := http_thrift.NewTHTTPServerFromMux(mux, "/base")
+	trans, err := http_thrift.NewTHTTPServerFromMux(mux, "/base")
+
+	if err != nil {
+		return nil, err
+	}
 
 	return &Endpoint{
 		servers: []*thrift.Server{
@@ -31,13 +35,19 @@ func NewEndpoint(baseHandler *handler.Base, port int) *Endpoint {
 		},
 		port: port,
 		mux:  mux,
-	}
+	}, nil
 }
 
-func (e *Endpoint) Mount(processor stdThrift.TProcessor, path string) {
-	trans, _ := http_thrift.NewTHTTPServerFromMux(e.mux, path)
+func (e *Endpoint) Mount(processor stdThrift.TProcessor, path string) error {
+	trans, err := http_thrift.NewTHTTPServerFromMux(e.mux, path)
+
+	if err != nil {
+		return err
+	}
 
 	e.servers = append(e.servers, thrift.NewServer(processor, trans))
+
+	return nil
 }
 
 func (e *Endpoint) Serve() error {
