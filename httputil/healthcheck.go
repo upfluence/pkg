7d19d2a9aa22c9752e1f906b2cec60bf09702e48
Alexis Montagne <alexis.montagne@gmail.com>
2017-03-21 11:20:04 -0400

    Fix up the healthcheck misspelling

--- httputil/healthcheck.go
+++ httputil/healthcheck.go
@@ -2,6 +2,6 @@ package httputil
 
 import "net/http"
 
-func HealcheckHander(w http.ResponseWriter, r *http.Request) {
+func HealthcheckHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("ok"))
 }
