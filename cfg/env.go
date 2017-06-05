a5db45ad7183568f68d7681d7837633c73df9df1
Alexis Montagne <alexis.montagne@gmail.com>
2017-06-05 16:50:30 -0400

    cfg: add the FetchStrings helper

--- cfg/env.go
+++ cfg/env.go
@@ -3,9 +3,12 @@ package cfg
 import (
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/upfluence/goutils/log"
 )
+
+const listSeparator = ","
 
 func FetchString(variable, defaultValue string) string {
 	if v := os.Getenv(variable); v != "" {
@@ -24,6 +27,14 @@ func FetchInt(variable string, defaultValue int) int {
 		}
 
 		log.Errorf("fetchInt: %s", err.Error())
+	}
+
+	return defaultValue
+}
+
+func FetchStrings(variable string, defaultValue []string) []string {
+	if v := os.Getenv(variable); v != "" {
+		return strings.Split(v, listSeparator)
 	}
 
 	return defaultValue
