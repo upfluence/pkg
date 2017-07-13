5dc071648195cd49f0b9098957a36d13973c8a15
Alexis Montagne <alexis.montagne@gmail.com>
2017-07-13 14:50:41 -0400

    Move the type of the options to map[string]interface{}

--- error_logger/error_logger.go
+++ error_logger/error_logger.go
@@ -1,21 +1,48 @@
 package error_logger
 
+import (
+	"os"
+
+	"github.com/upfluence/goutils/error_logger/noop"
+	"github.com/upfluence/goutils/error_logger/opbeat"
+	"github.com/upfluence/goutils/error_logger/sentry"
+)
+
 var DefaultErrorLogger ErrorLogger
 
-type Options map[string]interface{}
-
 type ErrorLogger interface {
-	Capture(error, *Options) error
+	Capture(error, map[string]interface{}) error
 	Close()
 }
 
-func Setup(logger ErrorLogger) {
-	DefaultErrorLogger = logger
+func init() {
+	if v := os.Getenv("SENTRY_DSN"); v != "" {
+		l, err := sentry.NewErrorLogger(v)
+
+		if err != nil {
+			DefaultErrorLogger = noop.NewErrorLogger()
+		} else {
+			DefaultErrorLogger = l
+		}
+	} else if v := os.Getenv("OPBEAT_APP_ID"); v != "" {
+		DefaultErrorLogger = opbeat.NewErrorLogger()
+	} else {
+		DefaultErrorLogger = noop.NewErrorLogger()
+	}
+
 	if e := recover(); e != nil {
 		if err, ok := e.(error); ok {
-			logger.Capture(err, nil)
-			logger.Close()
+			DefaultErrorLogger.Capture(err, nil)
+			DefaultErrorLogger.Close()
 			panic(err.Error())
 		}
 	}
+}
+
+func Capture(err error, opts map[string]interface{}) error {
+	return DefaultErrorLogger.Capture(err, opts)
+}
+
+func Close() {
+	DefaultErrorLogger.Close()
 }
