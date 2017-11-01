6f3b76cf0532b6bdd58d686c9e07bdd11f8d2293
tgallice <tm.gallice@gmail.com>
2017-11-01 17:16:17 +0100

    Extract decoder && log error

--- stringutil/decoder_test.go
+++ stringutil/decoder_test.go
@@ -7,19 +7,18 @@ import (
 
 func TestDecodeToUTF8(t *testing.T) {
 	for _, testcase := range []struct {
-		Value    string
-		Expected string
+		in, out string
 	}{
 		{"test", "test"},
 		{"testé", "testé"},
 		{"test\xc3", "testÃ"},
 		{"\xc3", "Ã"},
 	} {
-		if testcase.Expected != DecodeToUTF8(testcase.Value) {
+		if testcase.out != DecodeToUTF8(testcase.in) {
 			t.Errorf(
 				fmt.Sprintf(
-					"Fail to decode string '%x'",
-					testcase.Value,
+					"Fail to decode string '%s'",
+					testcase.in,
 				),
 			)
 
