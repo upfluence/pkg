4cd129d653bca520a3ca86590a304a4e2b84cc0f
Alexis Montagne <alexis.montagne@gmail.com>
2018-07-16 16:12:12 -0400

    log: Use github.com/upfluence/log

--- log/logger.go
+++ log/logger.go
@@ -1,111 +1,88 @@
 package log
 
 import (
-	"os"
+	"context"
 
-	"github.com/op/go-logging"
+	"github.com/upfluence/log"
+	"github.com/upfluence/log/record"
+	elsink "github.com/upfluence/log/sink/error_logger"
+	"github.com/upfluence/log/sink/multi"
+	"github.com/upfluence/log/sink/writer"
+
 	"github.com/upfluence/pkg/error_logger"
 )
 
-const (
-	defaultStdoutLevel      = logging.NOTICE
-	defaultErrorLoggerLevel = logging.ERROR
-)
-
 var (
-	logger = &logging.Logger{Module: "upfluence", ExtraCalldepth: 1}
-	format = logging.MustStringFormatter(
-		`[%{level:.1s} %{time:060102 15:04:05} %{shortfile}] %{message}`,
-	)
-
-	stdoutBackend = logging.AddModuleLevel(
-		logging.NewBackendFormatter(
-			logging.NewLogBackend(os.Stdout, "", 0),
-			format,
+	Logger = log.NewLogger(
+		log.WithSink(
+			multi.NewSink(
+				writer.NewStandardStdoutSink(2),
+				elsink.NewSink(error_logger.DefaultErrorLogger),
+			),
 		),
 	)
-
-	errBackend = logging.AddModuleLevel(
-		&errorLoggerBackend{client: error_logger.DefaultErrorLogger},
-	)
 )
 
-func init() {
-	var (
-		level logging.Level
-		err   error
-	)
-
-	if level, err = logging.LogLevel(os.Getenv("LOGGER_LEVEL")); err != nil {
-		level = defaultStdoutLevel
-	}
-
-	stdoutBackend.SetLevel(level, "")
-	errBackend.SetLevel(defaultErrorLoggerLevel, "")
-
-	logging.SetBackend(stdoutBackend, errBackend)
-}
-
 func Fatal(args ...interface{}) {
-	logger.Fatal(args...)
+	Logger.Fatal(args...)
 }
 
 func Fatalf(format string, args ...interface{}) {
-	logger.Fatalf(format, args...)
-}
-
-func Panic(args ...interface{}) {
-	logger.Panic(args...)
-}
-
-func Panicf(format string, args ...interface{}) {
-	logger.Panicf(format, args...)
-}
-
-func Critical(args ...interface{}) {
-	logger.Critical(args...)
-}
-
-func Criticalf(format string, args ...interface{}) {
-	logger.Criticalf(format, args...)
+	Logger.Fatalf(format, args...)
 }
 
 func Error(args ...interface{}) {
-	logger.Error(args...)
+	Logger.Error(args...)
 }
 
 func Errorf(format string, args ...interface{}) {
-	logger.Errorf(format, args...)
+	Logger.Errorf(format, args...)
 }
 
 func Warning(args ...interface{}) {
-	logger.Warning(args...)
+	Logger.Warning(args...)
 }
 
 func Warningf(format string, args ...interface{}) {
-	logger.Warningf(format, args...)
+	Logger.Warningf(format, args...)
 }
 
 func Notice(args ...interface{}) {
-	logger.Notice(args...)
+	Logger.Notice(args...)
 }
 
 func Noticef(format string, args ...interface{}) {
-	logger.Noticef(format, args...)
+	Logger.Noticef(format, args...)
 }
 
 func Info(args ...interface{}) {
-	logger.Info(args...)
+	Logger.Info(args...)
 }
 
 func Infof(format string, args ...interface{}) {
-	logger.Infof(format, args...)
+	Logger.Infof(format, args...)
 }
 
 func Debug(args ...interface{}) {
-	logger.Debug(args...)
+	Logger.Debug(args...)
 }
 
 func Debugf(format string, args ...interface{}) {
-	logger.Debugf(format, args...)
+	Logger.Debugf(format, args...)
+}
+
+func WithField(f record.Field) log.Logger {
+	return Logger.WithField(f)
+}
+
+func WithFields(fs ...record.Field) log.Logger {
+	return Logger.WithFields(fs...)
+}
+
+func WithContext(ctx context.Context) log.Logger {
+	return Logger.WithContext(ctx)
+}
+
+func WithError(err error) log.Logger {
+	return Logger.WithError(err)
 }
