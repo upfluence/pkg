08c4b2fdc6233bb4e301efdb42a8645d4c576305
Alexis Montagne <alexis.montagne@gmail.com>
2018-07-30 14:15:25 -0400

    backoff: Add some strategy wrapper (jitter, limit)

--- backoff/strategy.go
+++ backoff/strategy.go
@@ -1,9 +1,64 @@
 package backoff
 
-import "time"
+import (
+	"math/rand"
+	"time"
+)
 
-const Cancelled time.Duration = -1
+const (
+	Cancelled time.Duration = -1
+	Forever                 = -1
+)
 
 type Strategy interface {
 	Backoff(int) (time.Duration, error)
+}
+
+func LimitStrategy(s Strategy, l int) Strategy {
+	return &limitedStrategy{Strategy: s, limit: l}
+}
+
+type limitedStrategy struct {
+	Strategy
+
+	limit int
+}
+
+func (b *limitedStrategy) Backoff(n int) (time.Duration, error) {
+	if l := b.limit; l == Forever || n < l {
+		return b.Strategy.Backoff(n)
+	}
+
+	return Cancelled, nil
+}
+
+type StrategyFn func(int) time.Duration
+
+func (fn StrategyFn) Backoff(i int) (time.Duration, error) {
+	return fn(i), nil
+}
+
+func JitterStrategy(s Strategy, j time.Duration) Strategy {
+	return &jitterStrategy{
+		Strategy: s,
+		jitFn: func() time.Duration {
+			return time.Duration(rand.Int63n(int64(j)))
+		},
+	}
+}
+
+type jitterStrategy struct {
+	Strategy
+
+	jitFn func() time.Duration
+}
+
+func (s *jitterStrategy) Backoff(i int) (time.Duration, error) {
+	var d, err = s.Strategy.Backoff(i)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return d + s.jitFn(), nil
 }
