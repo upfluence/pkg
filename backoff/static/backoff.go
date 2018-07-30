76a164391834888c414121ea7d86bd93450352ac
Alexis Montagne <alexis.montagne@gmail.com>
2018-07-30 14:16:00 -0400

    backoff/static: Refactor this package with backoff utils

--- backoff/static/backoff.go
+++ backoff/static/backoff.go
@@ -6,25 +6,10 @@ import (
 	"github.com/upfluence/pkg/backoff"
 )
 
-const Forever = -1
-
-type Backoff struct {
-	retries    int
-	retryDelay time.Duration
+func NewBackoff(ts int, delay time.Duration) backoff.Strategy {
+	return backoff.LimitStrategy(NewInfiniteBackoff(delay), ts)
 }
 
-func NewBackoff(retries int, delay time.Duration) *Backoff {
-	return &Backoff{retries: retries, retryDelay: delay}
-}
-
-func NewInfiniteBackoff(delay time.Duration) *Backoff {
-	return &Backoff{retries: Forever, retryDelay: delay}
-}
-
-func (b *Backoff) Backoff(n int) (time.Duration, error) {
-	if b.retries == Forever || n < b.retries {
-		return b.retryDelay, nil
-	}
-
-	return backoff.Cancelled, nil
+func NewInfiniteBackoff(delay time.Duration) backoff.Strategy {
+	return backoff.StrategyFn(func(int) time.Duration { return delay })
 }
