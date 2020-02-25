658d2f35c1706f4a2899ec4cca5e8c028ce23d51
Alexis Montagne <alexis.montagne@gmail.com>
2020-02-25 18:19:12 -0500

    stringutil: Implement Batch function

---
+++ stringutil/batch.go
@@ -0,0 +1,17 @@
+package stringutil
+
+func Batch(slice []string, size int) [][]string {
+	if len(slice) == 0 {
+		return nil
+	}
+
+	batches := make([][]string, 0, (len(slice)+size-1)/size)
+
+	for size < len(slice) {
+		slice, batches = slice[size:], append(batches, slice[0:size:size])
+	}
+
+	batches = append(batches, slice)
+
+	return batches
+}
