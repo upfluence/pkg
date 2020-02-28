80a6f568c92b3c5f210327da97abc0feaa3ecd4e
Alexis Montagne <alexis.montagne@gmail.com>
2020-02-28 10:14:32 -0500

    group/limiter: Add package to tie together limiter and group

---
+++ group/limiter/group.go
@@ -0,0 +1,40 @@
+package limiter
+
+import (
+	"context"
+
+	"github.com/upfluence/pkg/group"
+	"github.com/upfluence/pkg/limiter"
+)
+
+type Group struct {
+	g group.Group
+	l limiter.Limiter
+
+	noWait bool
+}
+
+func WrapGroup(g group.Group, l limiter.Limiter) *Group {
+	return &Group{g: g, l: l}
+}
+
+func (g *Group) Do(r group.Runner) { g.g.Do(wrapRunner(r, g.l, g.noWait)) }
+func (g *Group) Wait() error       { return g.g.Wait() }
+
+func wrapRunner(r group.Runner, l limiter.Limiter, noWait bool) group.Runner {
+	return func(ctx context.Context) error {
+		var done, err = l.Allow(
+			ctx,
+			limiter.AllowOptions{N: 1, NoWait: noWait},
+		)
+
+		if err != nil {
+			return err
+		}
+
+		err = r(ctx)
+		done()
+
+		return err
+	}
+}
