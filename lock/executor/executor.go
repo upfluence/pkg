fde46971ebb3b95ece537f59fa6bd7bb30495918
Alexis Montagne <alexis.montagne@gmail.com>
2020-03-11 17:17:13 -0400

    lock/executor: Add executor pacakge to execute Task protected by a lock

---
+++ lock/executor/executor.go
@@ -0,0 +1,129 @@
+package executor
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	"github.com/upfluence/pkg/lock"
+	"github.com/upfluence/pkg/log"
+	"github.com/upfluence/pkg/timeutil"
+)
+
+var (
+	defaultLockDuration   = 30 * time.Second
+	defaultRenewInterfval = (1 / 3) * defaultLockDuration
+
+	defaultExecutorOptions = executorOptions{
+		lockDuration:  defaultLockDuration,
+		renewInterval: defaultRenewInterfval,
+		clock:         timeutil.Background(),
+	}
+)
+
+type executorOptions struct {
+	clock timeutil.Clock
+
+	lockDuration  time.Duration
+	renewInterval time.Duration
+
+	noWait bool
+	op     lock.OpType
+}
+
+func (eos executorOptions) deadline() time.Time {
+	return eos.clock.Now().Add(eos.lockDuration)
+}
+
+func (eos executorOptions) acquireOptions() lock.AcquireOptions {
+	return lock.AcquireOptions{
+		NoWait:   eos.noWait,
+		Deadline: eos.deadline(),
+		Op:       eos.op,
+	}
+}
+
+type Task func(context.Context) error
+type ExecutorOption func(*executorOptions)
+
+func WithLockDuration(d time.Duration) ExecutorOption {
+	return func(eos *executorOptions) { eos.lockDuration = d }
+}
+
+func WithRenewInterval(d time.Duration) ExecutorOption {
+	return func(eos *executorOptions) { eos.renewInterval = d }
+}
+
+type Executor struct {
+	l lock.Lock
+	t Task
+
+	executorOptions
+}
+
+func NewExecutor(l lock.Lock, t Task, opts ...ExecutorOption) *Executor {
+	var e = Executor{l: l, t: t, executorOptions: defaultExecutorOptions}
+
+	for _, opt := range opts {
+		opt(&e.executorOptions)
+	}
+
+	return &e
+}
+
+func (e *Executor) Execute(ctx context.Context) error {
+	var (
+		wg sync.WaitGroup
+
+		le, err      = e.l.Acquire(ctx, e.acquireOptions())
+		cctx, cancel = context.WithCancel(ctx)
+	)
+
+	if err != nil {
+		cancel()
+		return err
+	}
+
+	wg.Add(2)
+
+	go func() {
+		select {
+		case <-le.Done():
+			cancel()
+		case <-cctx.Done():
+		}
+
+		wg.Done()
+	}()
+
+	go func() {
+		t := e.clock.Timer(e.renewInterval)
+
+		for {
+			select {
+			case <-cctx.Done():
+				t.Stop()
+				wg.Done()
+				return
+			case <-t.C():
+				if kerr := le.KeepAlive(ctx, e.deadline()); kerr != nil {
+					log.WithError(kerr).Error("cant renew lock")
+				}
+			}
+
+			t.Reset(e.renewInterval)
+		}
+	}()
+
+	err = e.t(cctx)
+
+	cancel()
+
+	if rerr := le.Release(ctx); rerr != nil {
+		log.WithError(rerr).Error("cant release lock")
+	}
+
+	wg.Wait()
+
+	return err
+}
---
+++ lock/executor/executor_test.go
@@ -0,0 +1,28 @@
+package executor
+
+import (
+	"context"
+	"sync/atomic"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/upfluence/pkg/lock/local"
+)
+
+func TestExecute(t *testing.T) {
+	var (
+		i  int64
+		lm local.LockManager
+
+		err = NewExecutor(
+			lm.Lock("foo"),
+			func(context.Context) error {
+				atomic.AddInt64(&i, 1)
+				return nil
+			},
+		).Execute(context.Background())
+	)
+
+	assert.Nil(t, err)
+	assert.Equal(t, int64(1), i)
+}
