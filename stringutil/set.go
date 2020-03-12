5eb3840c4a4f0c8cde64a3291cbeb20635f4e9a5
Alexis Montagne <alexis.montagne@gmail.com>
2020-03-12 12:44:00 -0400

    stringutil: Rename StringSet in Set and add a type alias

---
+++ stringutil/set.go
@@ -0,0 +1,41 @@
+package stringutil
+
+import "sync"
+
+type StringSet = Set
+
+type Set struct {
+	sync.Once
+
+	Set map[string]struct{}
+}
+
+func (s *Set) Add(vs ...string) {
+	if len(vs) == 0 {
+		return
+	}
+
+	s.Do(func() {
+		if s.Set == nil {
+			s.Set = make(map[string]struct{}, len(vs))
+		}
+	})
+
+	for _, v := range vs {
+		s.Set[v] = struct{}{}
+	}
+}
+
+func (s *Set) Strings() []string {
+	if len(s.Set) == 0 {
+		return nil
+	}
+
+	res := make([]string, 0, len(s.Set))
+
+	for v := range s.Set {
+		res = append(res, v)
+	}
+
+	return res
+}
---
+++ stringutil/set_test.go
@@ -0,0 +1,51 @@
+package stringutil
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type assertStrings func(*testing.T, []string)
+
+var assertNil = func(t *testing.T, es []string) {
+	assert.Nil(t, es)
+}
+
+func TestSet(t *testing.T) {
+	for _, tCase := range []struct {
+		in     [][]string
+		assert assertStrings
+	}{
+		{
+			in:     [][]string{{"foo", "bar", "foo"}},
+			assert: assertElementsMatch("foo", "bar"),
+		},
+		{
+			in:     [][]string{{"foo"}, {"bar"}, {"foo"}},
+			assert: assertElementsMatch("foo", "bar"),
+		},
+		{
+			in:     [][]string{},
+			assert: assertNil,
+		},
+		{
+			in:     [][]string{{}},
+			assert: assertNil,
+		},
+	} {
+		var s Set
+
+		for _, b := range tCase.in {
+			s.Add(b...)
+		}
+
+		tCase.assert(t, s.Strings())
+	}
+}
+
+func assertElementsMatch(ss ...string) assertStrings {
+	return func(t *testing.T, es []string) {
+		assert.ElementsMatch(t, es, ss)
+	}
+}
