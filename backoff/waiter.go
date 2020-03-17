04f48ae1e1fb88cffc05d5812d958e72570f6ad0
Alexis Montagne <alexis.montagne@gmail.com>
2020-03-17 09:42:21 -0400

    backoff: Add option to change the clock in the Waiter

--- backoff/waiter.go
+++ backoff/waiter.go
@@ -10,6 +10,12 @@ import (
 
 var ErrCanceled = errors.New("backoff: strategy canceled")
 
+type WaiterOption func(*Waiter)
+
+func WithClock(c timeutil.Clock) WaiterOption {
+	return func(w *Waiter) { w.c = c }
+}
+
 type Waiter struct {
 	s Strategy
 	c timeutil.Clock
@@ -17,8 +23,14 @@ type Waiter struct {
 	i int32
 }
 
-func NewWaiter(s Strategy) *Waiter {
-	return &Waiter{s: s, c: timeutil.Background()}
+func NewWaiter(s Strategy, opts ...WaiterOption) *Waiter {
+	var w = Waiter{s: s, c: timeutil.Background()}
+
+	for _, opt := range opts {
+		opt(&w)
+	}
+
+	return &w
 }
 
 func (w *Waiter) Reset() { atomic.StoreInt32(&w.i, 0) }
