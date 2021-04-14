e4c2aa749aa3c583e3703efdfeb36f14f93d9645
Alexis Montagne <alexis.montagne@gmail.com>
2021-04-14 16:07:26 -0400

    multierror: Use upfluence/errors

--- multierror/multierror.go
+++ multierror/multierror.go
@@ -1,55 +1,11 @@
 package multierror
 
-import (
-	"fmt"
-	"strings"
-)
+import "github.com/pkg/errors"
 
-func Wrap(errs []error) error { return wrap(errs) }
+func Wrap(errs []error) error { return errors.Wrap(errs) }
 
-func Combine(errs ...error) error { return wrap(errs) }
+func Combine(errs ...error) error { return errors.Wrap(errs) }
 
 type MultiError interface {
 	Errors() []error
-}
-
-type multiError []error
-
-func wrap(errs []error) error {
-	var out []error
-
-	for _, err := range errs {
-		if err == nil {
-			continue
-		}
-
-		if merr, ok := err.(MultiError); ok {
-			out = append(out, merr.Errors()...)
-		} else {
-			out = append(out, err)
-		}
-	}
-
-	switch len(out) {
-	case 0:
-		return nil
-	case 1:
-		return out[0]
-	default:
-		return multiError(out)
-	}
-}
-
-func (errs multiError) Errors() []error {
-	return errs
-}
-
-func (errs multiError) Error() string {
-	parts := make([]string, len(errs))
-
-	for i, err := range errs {
-		parts[i] = err.Error()
-	}
-
-	return fmt.Sprintf("[%s]", strings.Join(parts, ", "))
 }
