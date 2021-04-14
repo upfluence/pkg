b31be5c45857b1c2674ffa89ded8676f9f2ef39a
Alexis Montagne <alexis.montagne@gmail.com>
2021-04-14 16:54:07 -0400

    multierror: Remove this package

--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.14
 require (
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/golang/snappy v0.0.2-0.20190904063534-ff6b7dc882cf
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/stretchr/testify v1.6.1
 	github.com/upfluence/errors v0.0.2
--- testutil/helpers.go
+++ testutil/helpers.go
@@ -3,23 +3,7 @@ package testutil
 import (
 	"os"
 	"testing"
-
-	"github.com/upfluence/errors/errtest"
 )
-
-type ErrorAssertion func(testing.TB, error)
-
-func NoError(msgAndArgs ...interface{}) ErrorAssertion {
-	return errtest.NoError(msgAndArgs...).Assert
-}
-
-func ErrorEqual(err error, msgAndArgs ...interface{}) ErrorAssertion {
-	return errtest.ErrorEqual(err, msgAndArgs...).Assert
-}
-
-func ErrorCause(err error, msgAndArgs ...interface{}) ErrorAssertion {
-	return errtest.ErrorCause(err, msgAndArgs...).Assert
-}
 
 func FetchEnvVariable(t testing.TB, ev string) string {
 	v := os.Getenv(ev)
