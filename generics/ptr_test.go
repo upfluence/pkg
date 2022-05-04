1735459f8c2da7de2d6e7ca0a26ae79adcf8987a
Sylvain Prost <sylvain.prost@upfluence.com>
2022-05-04 14:14:44 +0200

    generic: add helpers

---
+++ generics/ptr_test.go
@@ -0,0 +1,29 @@
+//go:build go1.18
+
+package generics
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPointer(t *testing.T) {
+	assert.Equal(
+		t,
+		"test",
+		*(Pointer("test")),
+	)
+}
+
+func TestReferenceSlice(t *testing.T) {
+	var (
+		i = 1
+		j = 2
+	)
+	assert.Equal(
+		t,
+		[]*int{&i, &j},
+		ReferenceSlice([]int{i, j}),
+	)
+}
