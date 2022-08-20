da4ddc8664056e783363fc1ca939df991a4ec631
Alexis Montagne <alexis.montagne@gmail.com>
2022-08-19 22:45:32 -0700

    iopool,pool: Make the Pool s generic

--- pool/pool.go
+++ pool/pool.go
@@ -2,29 +2,38 @@ package pool
 
 import (
 	"context"
-	"io"
+
+	"github.com/upfluence/pkg/iopool"
 )
 
-type Factory func(context.Context) (interface{}, error)
-
-type Pool interface {
-	io.Closer
-
-	Get(context.Context) (interface{}, error)
-	Put(interface{}) error
-	Discard(interface{}) error
+type entity[T comparable] struct {
+	Value T
 }
 
-type PoolFactory interface {
-	GetPool(Factory) Pool
+func (e entity[T]) Close() error               { return nil }
+func (e entity[T]) Open(context.Context) error { return nil }
+func (e entity[T]) IsOpen() bool               { return true }
+
+type Pool[T comparable] struct {
+	p *iopool.Pool[entity[T]]
 }
 
-type IntrospectablePool interface {
-	Pool
-
-	GetStats() (int, int)
+func NewPool[T comparable](newfn func() T, opts ...iopool.Option) *Pool[T] {
+	return &Pool[T]{
+		p: iopool.NewPool[entity[T]](
+			func(context.Context) (entity[T], error) {
+				return entity[T]{Value: newfn()}, nil
+			},
+			opts...,
+		),
+	}
 }
 
-type IntrospectablePoolFactory interface {
-	GetIntrospectablePool(Factory) IntrospectablePool
+func (p *Pool[T]) Get(ctx context.Context) (T, error) {
+	var v, err = p.p.Get(ctx)
+
+	return v.Value, err
 }
+
+func (p *Pool[T]) Put(v T) error     { return p.p.Put(entity[T]{Value: v}) }
+func (p *Pool[T]) Discard(v T) error { return p.p.Discard(entity[T]{Value: v}) }
