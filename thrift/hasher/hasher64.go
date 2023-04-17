2f1852a55ba8f967ff7fc0583f43c5cee249034c
Alexis Montagne <alexis.montagne@gmail.com>
2023-04-17 15:13:58 -0700

    thrift/hasher: Add Hasher64 struct

---
+++ thrift/hasher/hasher64.go
@@ -0,0 +1,39 @@
+package hasher
+
+import (
+	"hash"
+	"hash/fnv"
+	"sync"
+
+	"github.com/upfluence/pkg/thrift/serializer"
+	"github.com/upfluence/thrift/lib/go/thrift"
+)
+
+type Hasher64 struct {
+	hasherPool sync.Pool
+	serializer serializer.Serializer
+}
+
+func NewFNVHasher64() *Hasher64 { return NewHasher64(fnv.New64) }
+
+func NewHasher64(fn func() hash.Hash64) *Hasher64 {
+	return &Hasher64{
+		hasherPool: sync.Pool{New: func() any { return fn() }},
+		serializer: serializer.NewTSerializer(thrift.NewTBinaryProtocolFactoryDefault()),
+	}
+}
+
+func (h *Hasher64) Hash(msg serializer.TStruct) (uint64, error) {
+	hh := h.hasherPool.Get().(hash.Hash64)
+
+	defer func() {
+		hh.Reset()
+		h.hasherPool.Put(hh)
+	}()
+
+	if err := h.serializer.WriteTo(msg, hh); err != nil {
+		return 0, err
+	}
+
+	return hh.Sum64(), nil
+}
---
+++ thrift/hasher/hasher64_test.go
@@ -0,0 +1,52 @@
+package hasher
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/upfluence/pkg/thrift/serializer"
+	"github.com/upfluence/thrift/lib/go/thrift"
+)
+
+type stringTStruct struct {
+	s string
+}
+
+func (sts *stringTStruct) Write(p thrift.TProtocol) error {
+	return p.WriteString(sts.s)
+}
+
+func (sts *stringTStruct) Read(p thrift.TProtocol) error {
+	panic("unexpected")
+}
+
+func TestHash(t *testing.T) {
+	for _, tt := range []struct {
+		name string
+		in   serializer.TStruct
+		out  uint64
+	}{
+		{
+			name: "string",
+			in:   &stringTStruct{s: "foo"},
+			out:  0x81813814bc89acf6,
+		},
+		{
+			name: "list string",
+			in: serializer.SliceWriter{
+				&stringTStruct{s: "foo"},
+				&stringTStruct{s: "bar"},
+			},
+			out: 0xd4c58286fef8654,
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			h := NewFNVHasher64()
+
+			res, err := h.Hash(tt.in)
+
+			assert.NoError(t, err)
+			assert.Equal(t, tt.out, res)
+		})
+	}
+}
