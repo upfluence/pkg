f337b5b99c2efb8419b680524b9d50533f27a159
Xavier Petit <xavier.petit@upfluence.com>
2023-10-04 18:35:33 +0200

    Update generics/ptr_test.go

--- generics/ptr_test.go
+++ generics/ptr_test.go
@@ -17,11 +17,11 @@ func TestPointer(t *testing.T) {
 }
 
 func TestNullablePtr(t *testing.T) {
-	assert.Equal(t, -5,  *NullablePtr(-5))
+	assert.Equal(t, -5, *NullablePtr(-5))
 	assert.Nil(t, NullablePtr(0))
 
-	assert.Equal(t, *NullablePtr[string]("string"), "string")
-	assert.Equal(t, NullablePtr[string](""), (*string)(nil))
+	assert.Equal(t, *NullablePtr("string"), "string")
+	assert.Nil(t, NullablePtr(""))
 }
 
 func TestReferenceSlice(t *testing.T) {
