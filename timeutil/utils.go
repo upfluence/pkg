cd440fd9a7d7043ff2c6f934420f625a12c2a794
tgallice <tmgallice@gmail.com>
2023-10-05 18:31:56 +0200

    timeutil: Add an helper for get the unix or nil value

---
+++ timeutil/utils.go
@@ -0,0 +1,16 @@
+package timeutil
+
+import (
+	"time"
+
+	"github.com/upfluence/pkg/pointers"
+)
+
+// UnixOrrNil returns the unix timestamp or nil if time is zero.
+func UnixOrNil(t time.Time) *int64 {
+	if t.IsZero() {
+		return nil
+	}
+
+	return pointers.Ptr(t.Unix())
+}
