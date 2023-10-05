a9252b9ccf056e3e4c2d1fab4ee37a0e06dbef61
Xavier Petit <xavier.petit@upfluence.com>
2023-10-05 09:55:52 +0200

    Deprecate non-generic functions

--- int64util/ptrutil.go
+++ int64util/ptrutil.go
@@ -1,5 +1,6 @@
 package int64util
 
+// Deprecated: Prefer using generics.NullablePtr().
 func NullablePtr(i int64) *int64 {
 	if i == 0 {
 		return nil
--- stringutil/ptrutil.go
+++ stringutil/ptrutil.go
@@ -1,5 +1,6 @@
 package stringutil
 
+// Deprecated: Prefer using generics.NullablePtr().
 func NullablePtr(s string) *string {
 	if s == "" {
 		return nil
