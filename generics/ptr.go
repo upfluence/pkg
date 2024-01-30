0160e3713e49eab011751f7f89188580618faa0a
Xavier Petit <xavier.petit@upfluence.com>
2024-01-30 10:02:26 +0100

    Fix deprecated comments

--- generics/ptr.go
+++ generics/ptr.go
@@ -7,12 +7,14 @@ package generics
 import "github.com/upfluence/pkg/slices"
 
 // Pointer pointers
+//
 // Deprecated: Use pointers.Ptr.
 func Pointer[T comparable](v T) *T {
 	return &v
 }
 
 // NullablePtr return nil if the value is zero
+//
 // Deprecated: Use pointers.NullablePtr.
 func NullablePtr[T comparable](v T) *T {
 	var zero T
@@ -26,12 +28,14 @@ func NullablePtr[T comparable](v T) *T {
 
 // ReferenceSlice returns slice with the value of the slice in params as
 // pointers.
+//
 // Deprecated: use slices.References instead.
 func ReferenceSlice[T comparable](s []T) []*T {
 	return slices.References(s)
 }
 
 // IndirectSlice
+//
 // Deprecated: use slices.Indirect instead.
 func IndirectSlice[T comparable](s []*T) []T {
 	return slices.Indirect(s)
