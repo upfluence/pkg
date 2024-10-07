c93d47ec84634d2dfeb39ecdb807f9f0e891dfa0
Sylvain Prost <sylvain.prost@upfluence.com>
2024-10-07 18:03:22 +0200

    pkg/slices: remove Sum helper

--- slices/reduce.go
+++ slices/reduce.go
@@ -1,7 +1,5 @@
 package slices
 
-import "golang.org/x/exp/constraints"
-
 // ReduceFrom will iterate over each element of slice and call reduceFn for each.
 // The return value of each reduceFn is passed to the next call of reduceFn as acc, or
 // in other term at each iteration on the slice.
@@ -19,12 +17,4 @@ func Reduce[S ~[]E, E any, A any](slice S, reduceFn func(acc A, elem E) A) A {
 	var acc A
 
 	return ReduceFrom(slice, acc, reduceFn)
-}
-
-type sumable interface {
-	constraints.Integer | constraints.Float | ~string
-}
-
-func Sum[T sumable](i, j T) T {
-	return i + j
 }
--- slices/reduce_test.go
+++ slices/reduce_test.go
@@ -40,18 +40,3 @@ func TestReduce(t *testing.T) {
 		assert.Equal(t, tt.want, got)
 	}
 }
-
-func TestSum(t *testing.T) {
-	t.Run("integer", func(t *testing.T) {
-		assert.Equal(t, 3, Reduce([]int{1, 2}, Sum))
-		assert.Equal(t, 25, ReduceFrom([]int{1, 2}, 22, Sum))
-	})
-	t.Run("float", func(t *testing.T) {
-		assert.Equal(t, 3.4, Reduce([]float64{1.1, 2.3}, Sum))
-		assert.Equal(t, 25.52, ReduceFrom([]float64{1.1, 2.2}, 22.22, Sum))
-	})
-	t.Run("string", func(t *testing.T) {
-		assert.Equal(t, "foobar", Reduce([]string{"foo", "bar"}, Sum))
-		assert.Equal(t, "buzfoobar", ReduceFrom([]string{"foo", "bar"}, "buz", Sum))
-	})
-}
